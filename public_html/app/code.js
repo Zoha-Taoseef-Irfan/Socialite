7bc5be1510df8766a442f55a8e6cf052833029da
TaoseefAziz <taoseefaziz@arizona.edu>
2023-05-01 20:50:43 -0700

    minor code var naming changes and major css changes

--- public_html/app/code.js
+++ public_html/app/code.js
@@ -5,10 +5,10 @@ function getPosts() {
   let p = fetch(url);
   let ps = p.then( (response) => {
     return response.json();
-  }).then((objects) => { 
+  }).then((posts) => { 
     let html = '';
-    for (i in objects) {
-      html += generatePostHTML(objects[i].username, objects[i].dateCreated, objects[i].text, objects[i].comments, objects[i].avatar);
+    for (i in posts) {
+      html += generatePostHTML(posts[i].username, posts[i].dateCreated, posts[i].text, posts[i].comments, posts[i].avatar);
       // TODO add button for like, and comment functionality here
     }
     let x = document.getElementById('posts');
@@ -42,4 +42,4 @@ function createPost() {
   });
 }
 
-setInterval(getPosts, 1000);
+// setInterval(getPosts, 1000);
--- public_html/app/common.js
+++ public_html/app/common.js
@@ -20,7 +20,11 @@ function generatePostHTML(username, date, postText, comments, img) {
     const usernameDateDiv = document.createElement('div');
     const postTextDiv = document.createElement('div');
     const avatarImg = document.createElement('img');
-  
+
+    const button = document.createElement('button');
+    const icon = document.createElement('span');
+    const text = document.createElement('span');
+
     // Change appearance
     avatarImg.width = 100;
     avatarImg.height = 100;
@@ -33,21 +37,30 @@ function generatePostHTML(username, date, postText, comments, img) {
     postTextDiv.className = 'posttext';
     postCommentsDiv.className = 'postComments';
     avatarImg.className= 'post_avatar';
-  
+    button.classList.add('fb-like-button');
+    icon.classList.add('fb-like-icon');
+    text.classList.add('fb-like-text');
+
     // Set content
     usernameDateDiv.textContent = `${username} ${date}`;
     postTextDiv.textContent = postText;
     postCommentsDiv.textContent = comments.join('\n');
     avatarImg.src = img;
+    text.textContent = '0';
   
     // Append the child elements
     postHeaderDiv.appendChild(avatarImg);
     postHeaderDiv.appendChild(usernameDateDiv);
     postContentDiv.appendChild(postTextDiv);
+
+    // Add the icon and text elements to the button
+    button.appendChild(icon);
+    button.appendChild(text);
   
     postDiv.appendChild(postHeaderDiv);
     postDiv.appendChild(postContentDiv);
     postDiv.appendChild(postCommentsDiv);
+    postDiv.appendChild(button);
     
     // Return the HTML string
     return postDiv.outerHTML;
