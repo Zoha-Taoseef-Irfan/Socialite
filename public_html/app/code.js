830ffa3c7a4ce87953e9b31136247933e7ff2b95
amimul1 <amimulehsanzoha@gmail.com>
2023-05-03 21:51:06 -0700

    comment

--- public_html/app/code.js
+++ public_html/app/code.js
@@ -1,4 +1,23 @@
-// hello
+ /* 
+     Author: Amimul Ehsan Zoha; Taoseef Aziz, Irfan Ahmad
+     Course: CS337 Spring 2023
+     Project: Socialite Social Media
+     It is the client of full stack application using html,css , javascript, express and node.js and MongoDb which is a 
+     clone of will be a social networking web app, similar to that of Facebook. (Our version is called Socialite)
+     The app have the primary functionality in which the users can create accounts and login to their individual accounts. 
+     For login, there is some sort of authentication mechanism. Once a user logs in, he will be assigned a session and cookies
+     will be used to keep track of a time after which this session will be over and redirected to the login page. After logging in, 
+     the user is redirected to the home page, which will be similar to that of a facebook. The home page will contain contents like the timeline of recent posts (of all users), 
+     and each post will have the option to be liked and commented on. There will be the option to upload images in a post. Users can be friends with each other. 
+     Users will be able to chat in a global chatroom messaging app inside of Socialite. There will be another page to view the
+     users own profile photo, his information and bio. The post images are nicely styled, they have hovering effect, and other
+     nice styling additions. The client serves sure of all this functionality by making different requests to the server
+     using fetch api. The server has well defined MongoDB schemas which outline the whole database of the socialite app. 
+     The experience of developing this large scale project was difficult, yet worthwhile and an incredible learning experience 
+     in which we gained mastery over making server using express framework, designing large scale web app, working with mongo BD 
+     and making requests using the Fetch API. We stramlined collaboration with Github version control. 
+
+  */
 function getPosts() {
   let url = '/posts/';
   // TODO go back and change the /categories/ route to /posts/
